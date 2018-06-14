9e86ce1ec53670e2f9a6afcb06ca93c532f0fb41
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-06-14 17:21:03 -0400

    input field with state props

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    placeName: ''
+  }
+
+  placeNameChangedHandler = (event) => {
+    alert(event);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello there!</Text>
+        <TextInput
+         style={{width: 300, borderColor: "black", borderWidth: 1}} 
+         value={this.state.placeName}
+         onChangeText={this.placeNameChangedHandler}/>        
       </View>
     );
   }
