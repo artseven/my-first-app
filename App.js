af245fdbb8592f44ef000dba084571da1fdd282f
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-06-15 14:51:59 -0400

    looks done

--- App.js
+++ App.js
@@ -17,12 +17,14 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <View style={styles.inputContainer}>
           <TextInput
-          style={{width: 300}}
           placeholder="An awesome place" 
           value={this.state.placeName}
           onChangeText={this.placeNameChangedHandler}
+          style={styles.placeInput}
           />
-          <Button title="Add" />
+          <Button 
+           title="Add"
+           style={styles.placeButton} />
         </View>
       </View>
     );
@@ -38,8 +40,17 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
   },
   inputContainer: {
-    flex: 1,
+    // flex: 1,
+    width: "100%",
     flexDirection: "row",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  placeInput: {
+    width: "70%"
+  },
+  placeButton: {
+    width: "30%"
+
   }
 });
