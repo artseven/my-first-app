6cf610f5135fe91f0cf49558a06863fdb654ebcc
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-06-15 14:41:59 -0400

    adding styles

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
   state = {
@@ -15,11 +15,15 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <TextInput
-         style={{width: 300}}
-         placeholder="An awesome place" 
-         value={this.state.placeName}
-         onChangeText={this.placeNameChangedHandler}/>        
+        <View style={styles.inputContainer}>
+          <TextInput
+          style={{width: 300}}
+          placeholder="An awesome place" 
+          value={this.state.placeName}
+          onChangeText={this.placeNameChangedHandler}
+          />
+          <Button title="Add" />
+        </View>
       </View>
     );
   }
@@ -33,4 +37,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between"
+  }
 });
