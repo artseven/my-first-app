9425b388ade5f8b3e322b306d6daebadcba402fe
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-07-02 15:14:39 -0400

    changes

--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -23,7 +23,7 @@ class PlaceInput extends Component {
           <Button 
            title="Add"
            style={styles.placeButton}
-           onPress={this.onPlaceAdded}
+           onPress={() => this.onPlaceAdded{this.state.placeName}}
           />
         </View>
     }
